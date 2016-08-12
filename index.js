dd1fcd77edcbb7b87b9c0b397911f121b58a1ec3
Francois Campbell <campbell.francois@gmail.com>
2016-08-12 12:43:32 -0400

    Fixed delays by changing map to list

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ var queryParams = {
 movieApi.getMovies(queryParams, function(movies) {
 	var desiredMovies = selectMovies(movies);
 	var schedules = scheduleGenerator.generateSchedules(desiredMovies, chooseParameters());
-	// console.log(util.inspect(schedules, 2));
-	// console.log(JSON.stringify(schedules, null, 2));
+
 	schedules.forEach(function(schedule, index) {
 		console.log('Schedule ' + (index + 1) + ' at ' + schedule.theatre.name);
-		schedule.showtimes.forEach(function(showtime) {
+		schedule.showtimes.forEach(function(showtime, showtimeIndex, showtimeArray) {
 			console.log('\t' + showtime.dateTime.toString() + ': ' + showtime.movie.title);
+			if (showtimeIndex < showtimeArray.length - 1) {
+				console.log('\tDelay of ' + schedule.delays[showtimeIndex] / 1000 / 60 + ' minutes ');
+			}
 		});
 	});
 });
--- schedule.js
+++ schedule.js
@@ -7,20 +7,17 @@ function Schedule(showtimes, theatre) {
 	this.showtimes = _.slice(showtimes);
 	this.theatre = theatre;
 	this.delays = this.calculateDelays();
-	this.totalDelay = _.sum(this.delays.values());
+	this.totalDelay = _.sum(this.delays);
 }
 
 Schedule.prototype.calculateDelays = function() {
-	var delays = new Map();
-	
+	var delays = [];	
 	this.showtimes.forEach(function(showtime, index, array) {
-		if (index >= array.length - 1) {
-			return;
+		if (index < array.length - 1) {
+			var endTime = moment(showtime.dateTime).add(showtime.movie.runTime);
+			var nextStartTime = array[index + 1].dateTime;
+			delays.push(nextStartTime.diff(endTime));
 		}
-		var endTime = moment(showtime.dateTime).add(showtime.movie.runTime);
-		var nextStartTime = array[index + 1].dateTime;
-		delays.set(showtime, nextStartTime.diff(endTime));
 	});
-
 	return delays;
 };
