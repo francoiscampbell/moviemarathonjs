e5dafa9104d5bd367feab8c85dcb921a72ad8592
Francois Campbell <campbell.francois@gmail.com>
2016-08-12 12:29:10 -0400

    Working; removed commented out code, console logging, and inlined a few variables

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const util = require('util');
-
 var movieApi = require('./movieApi');
 var movieApiKey = require('./apiKey');
 var moment = require('moment');
@@ -15,7 +13,13 @@ movieApi.getMovies(queryParams, function(movies) {
 	var desiredMovies = selectMovies(movies);
 	var schedules = scheduleGenerator.generateSchedules(desiredMovies, chooseParameters());
 	// console.log(util.inspect(schedules, 2));
-	console.log(JSON.stringify(schedules, null, 2));
+	// console.log(JSON.stringify(schedules, null, 2));
+	schedules.forEach(function(schedule, index) {
+		console.log('Schedule ' + (index + 1) + ' at ' + schedule.theatre.name);
+		schedule.showtimes.forEach(function(showtime) {
+			console.log('\t' + showtime.dateTime.toString() + ': ' + showtime.movie.title);
+		});
+	});
 });
 
 function chooseParameters() {
--- scheduleGenerator.js
+++ scheduleGenerator.js
@@ -1,5 +1,3 @@
-const util = require('util');
-
 var _ = require('lodash');
 var moment = require('moment');
 var Schedule = require('./schedule');
@@ -25,8 +23,7 @@ module.exports = {
 		var currentPermutation = [];
 
 		possibleTheatres.forEach(function(theatre) {
-			// console.log('\n' + theatre.name);
-			generateSchedule(theatre, movies, options.earliestTime, possibleSchedules, currentPermutation, options);
+			generateScheduleRecursive(theatre, movies, options.earliestTime, possibleSchedules, currentPermutation, options);
 		});
 
 		if (options.sortByDelay) {
@@ -36,43 +33,29 @@ module.exports = {
 	}
 };
 
-function generateSchedule(theatre, movies, startTime, possibleSchedules, currentPermutation, options) {
-	// console.log(theatre.name);
-	// console.log(movies);
-	// console.log('startTime: ' + startTime.toString());
-	// console.log(_.map(currentPermutation, function(showtime) { return showtime.dateTime.toString() }));
-
+function generateScheduleRecursive(theatre, movies, startTime, possibleSchedules, currentPermutation, options) {
 	if (!movies.length && currentPermutation.length) {
 		var currentSchedule = new Schedule(currentPermutation, theatre);
 		if (validateSchedule(currentSchedule, options)) {
 			possibleSchedules.push(currentSchedule);
-			// console.log(_.map(possibleSchedules, function(permutation) { return permutation.showtimes.toString() }));
-			// console.log('found a schedule');
-			// console.log(util.inspect(possibleSchedules, 2));
-			// console.log('possibleSchedules.length: ' + possibleSchedules.length);
 		}
 		return;
 	}
 	movies.forEach(function(movie) {
-		// console.log('movie: ' + movie.title);
-
 		var showtime = null
 		var nextAvailableStartTime = startTime
-
 		while ((showtime = findNextShowtimeForMovie(theatre, movie, nextAvailableStartTime, options))) {
-			// console.log('showtime: ' + showtime.dateTime.toString());
-
 			currentPermutation.push(showtime);
 			var remainingMovies = _.filter(movies, function(m) { return m.tmsId !== movie.tmsId });
 			nextAvailableStartTime = moment(showtime.dateTime).add(options.skipPreviews ? options.previewsLength : 0).add(movie.runTime);
-			generateSchedule(theatre, remainingMovies, nextAvailableStartTime, possibleSchedules, currentPermutation, options);
+			generateScheduleRecursive(theatre, remainingMovies, nextAvailableStartTime, possibleSchedules, currentPermutation, options);
 			currentPermutation.pop();
 		}
 	});
 }
 
 function reorganizeMoviesIntoModel(movies) {
-	var allTheatres = []
+	var allTheatres = [];
 
 	movies.forEach(function(movie) {
 		movie.showtimes.forEach(function(showtime) {
@@ -99,10 +82,6 @@ function reorganizeMoviesIntoModel(movies) {
 }
 
 function calculatePossibleTheatres(allTheatres, movies) {
-	// console.log(allTheatres);
-	// console.log('\n');
-	// console.log(movies);
-
 	return _.filter(allTheatres, function(theatre) {
 		var moviesAtThisTheatre = _.map(theatre.showtimes, function(showtime) {
 			return showtime.movie
@@ -113,36 +92,20 @@ function calculatePossibleTheatres(allTheatres, movies) {
 }
 
 function validateSchedule(schedule, options) {
-	return (!options.maxTotalDelay
-		|| schedule.totalDelay.asMilliseconds() < options.maxTotalDelay.asMilliseconds())
-	&& (!options.maxIndividualDelay 
-		|| !schedule.delays.length
-		|| _.max(schedule.delays.values()).asMilliseconds() < options.maxIndividualDelay.asMilliseconds());
+	var totalDelayValid = !options.maxTotalDelay || schedule.totalDelay.asMilliseconds() < options.maxTotalDelay.asMilliseconds();
+	var individualDelayValid = (!options.maxIndividualDelay || !schedule.delays.length || _.max(schedule.delays.values()).asMilliseconds() < options.maxIndividualDelay.asMilliseconds());
+	return totalDelayValid && individualDelayValid; 
 }
 
 function findNextShowtimeForMovie(theatre, movie, nextAvailableStartTime, options) {
-	// console.log('movie: ' + movie.title);
-	// console.log('nextAvailableStartTime: ' + nextAvailableStartTime.toString());
-
-	var foundShowtime = _.find(theatre.showtimes, function(showtime) {
-		var valid = showtime.movie.tmsId === movie.tmsId && validateShowtime(showtime, nextAvailableStartTime, options);
-
-		// console.log('showtime.dateTime: ' + showtime.dateTime.toString());
-		// console.log('showtime.movie: ' + showtime.movie.title);
-		// console.log('valid: ' + valid);
-		return valid;
+	return _.find(theatre.showtimes, function(showtime) {
+		return showtime.movie.tmsId === movie.tmsId && validateShowtime(showtime, nextAvailableStartTime, options);
 	});
-	if (foundShowtime) {
-		// console.log('foundShowtime.dateTime: ' + foundShowtime.dateTime.toString());
-	}
-	return foundShowtime;
 }
 
 function validateShowtime(showtime, nextAvailableStartTime, options) {
 	var startTimeValid = showtime.dateTime.isSameOrAfter(moment(nextAvailableStartTime).subtract(options.maxOverlap));
 	var endTimeValid = options.latestTime ? moment(showtime.dateTime).add(showtime.runTime).isSameOrBefore(options.latestTime) : true;
 
-	// console.log('startTimeValid: ' + startTimeValid);
-	// console.log('endTimeValid: ' + endTimeValid);
 	return startTimeValid && endTimeValid;
 }
